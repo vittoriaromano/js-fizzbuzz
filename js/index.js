371554419d4bc2f93a31533309513389c5cd7d7d
vittoriaromano <vittoria5005@gmail.com>
2023-02-23 20:11:13 +0400

    milestone-due

--- js/index.js
+++ js/index.js
@@ -3,28 +3,53 @@
 
 
 
+///dare al contenitore una variabile
+const ul = document.getElementById('lista');
+
 //creo un ciclo con la sintassi giusta fino a 101
 for (let i= 1; i < 101; i++) {
+
+    //dare al contenuto una variabile
+    let li = document.createElement('li');
+
     //se ha la convenzione dei mutipli di 3 e 5 fizzbuzz
     if ((i % 3 === 0) && (i % 5 === 0)){
         console.log('fizzbuzz');
-        //se no solo i mutipli di 5 fizzbuzz
+
+        //usando append dare al contenuto il valore della variabile 
+        li.append('fizzbuzz')
+        ul.append(li)
+
+    //se no solo i mutipli di 5 buzz
      }else if (i % 5 === 0) {
         console.log('buzz');
-        //se no solo i mutipli di 3  fizz
+        //usando append dare al contenuto il valore della variabile 
+        li.append('buzz')
+        ul.append(li)
+
+    //se no solo i mutipli di 3  fizz
      } else if(i % 3 === 0) {
         console.log('fizz');
+
+        //usando append dare al contenuto il valore della variabile 
+        li.append('fizz')
+        ul.append(li)
+
      }//altrimenti resta un numero
      else {
         console.log(i);
+        
+        //usando append dare al contenuto il valore della variabile 
+        li.append(i)
+        ul.append(li)
      }
-     
 
 }
 
 
 
 
+  
 
 
 
--- readme.md
+++ readme.md
@@ -1,15 +1,44 @@
+1)
 Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+-//creo un ciclo con la sintassi giusta fino a 101
+
+-//se di mutipli di 3 e 5 avra un'altra convenzione
+
+-//che abbia la convenzione if dei mutipli di 3
+
+-//ad anche la convenzione if dei mutipli di 5
 
 
-//creo un ciclo con la sintassi giusta fino a 101
 
-//se di mutipli di 3 e 5 avra un'altra convenzione
+2)
+ Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 
 
-//che abbia la convenzione if dei mutipli di 3
 
-//ad anche la convenzione if dei mutipli di 5
+-//dare al contenitore una variabile
+-//dare al contenuto una variabile
+-//usando append dare al contenitore il valore della variabile 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
